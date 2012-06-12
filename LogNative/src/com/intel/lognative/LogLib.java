cfd3da6e0dce0149d80d7add9f635c24c8a05a3b
Marko Gargenta <marko@marakana.com>
2012-06-11 19:52:18 -0700

    Added comments

--- LogNative/src/com/intel/lognative/LogLib.java
+++ LogNative/src/com/intel/lognative/LogLib.java
@@ -1,13 +1,16 @@
 package com.intel.lognative;
 
 public class LogLib {
-	
+
+	// Public Constants
 	public static final int DEBUG = 3;
-	public static final int WARN  = 5;
-	public static final int ERROR  = 6;
-	
+	public static final int WARN = 5;
+	public static final int ERROR = 6;
+
+	// API
 	public static native void log(int priority, String tag, String message);
-	
+
+	// Load the native library
 	static {
 		System.loadLibrary("loglib");
 	}
