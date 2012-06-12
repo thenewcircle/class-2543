9e8af89f6cabb88ab34e11b647be9942d6441ae0
Marko Gargenta <marko@marakana.com>
2012-06-11 21:03:14 -0700

    Added FibService

---
+++ FibService/src/com/intel/fibservice/IFibServiceImpl.java
@@ -0,0 +1,29 @@
+package com.intel.fibservice;
+
+import android.os.RemoteException;
+
+import com.intel.fibcommon.IFibService;
+
+public class IFibServiceImpl extends IFibService.Stub {
+
+	public long fibJ(long arg0) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public long fibJI(long arg0) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public long fibN(long arg0) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public long fibNI(long arg0) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
