ae4af00e9eb951ac4498ba56a801e8e4a1e0fd1f
Marko Gargenta <marko@marakana.com>
2012-06-12 01:48:03 -0700

    Implemented LogClient

--- LogService/src/com/intel/lognative/LogLib.java
+++ LogService/src/com/intel/lognative/LogLib.java
@@ -2,11 +2,6 @@ package com.intel.lognative;
 
 public class LogLib {
 
-	// Public Constants
-	public static final int DEBUG = 3;
-	public static final int WARN = 5;
-	public static final int ERROR = 6;
-
 	// API
 	public static native void log(int priority, String tag, String message);
 
