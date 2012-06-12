c42d2cb5ebd9e1a973b14409f85649b7c81a6f6c
Marko Gargenta <marko@marakana.com>
2012-06-11 19:15:54 -0700

    Implemented LogActivity

--- LogNative/src/com/intel/lognative/LogActivity.java
+++ LogNative/src/com/intel/lognative/LogActivity.java
@@ -2,12 +2,40 @@ package com.intel.lognative;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class LogActivity extends Activity {
+	EditText tag, message;
+	RadioGroup priority;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        tag = (EditText) findViewById(R.id.tag);
+        message = (EditText) findViewById(R.id.message);
+        priority = (RadioGroup) findViewById(R.id.priority);
+    }
+    
+    /** Called when button Log! is clicked. */
+    public void onClickLog(View v) {
+    		String tagString = tag.getText().toString();
+    		String messageString = message.getText().toString();
+    		int priorityInt = -1;
+    		
+    		switch( priority.getCheckedRadioButtonId() ) {
+    		case R.id.radio_debug:
+    			priorityInt = LogLib.DEBUG; break;
+    		case R.id.radio_warn:
+    			priorityInt = LogLib.WARN; break;
+    		case R.id.radio_error:
+    			priorityInt = LogLib.ERROR; break;
+    		}
+    		
+    		LogLib.log(priorityInt, tagString, messageString);
     }
 }
--- LogNative/src/com/intel/lognative/LogLib.java
+++ LogNative/src/com/intel/lognative/LogLib.java
@@ -1,6 +1,14 @@
 package com.intel.lognative;
 
 public class LogLib {
+	
+	public static final int DEBUG = 3;
+	public static final int WARN  = 5;
+	public static final int ERROR  = 6;
+	
 	public static native void log(int priority, String tag, String message);
-	/* You'll need to load your library here */
+	
+	static {
+		System.loadLibrary("loglib");
+	}
 }
